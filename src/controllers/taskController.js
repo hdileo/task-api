5418414840274f2816d9566ae768eec47e0ae8e3
Henry Di Leo <hdileo@charlotte.edu>
2026-04-14 22:46:08 -0400

    Revised code to meet assignment 10 instructions

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,7 +1,15 @@
 import * as taskService from '../services/taskService.js';
 
 export async function getTasks(req, res, next) {
-  const tasks = await taskService.getAllTasks();
+  const { completed } = req.query;
+
+  let completedFilter;
+
+  if (completed !== undefined) {
+    completedFilter = completed === 'true';
+  }
+
+  const tasks = await taskService.getAllTasks(completedFilter);
   res.json(tasks);
 }
 
